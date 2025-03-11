9c63e3ade3d818441d51f95b8be748dae3fa8993
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-03-11 17:06:25 +0100

    feat/no-ref/implement-filehandler

--- file_handler.py
+++ file_handler.py
@@ -11,17 +11,35 @@ class FileHandler:
         return [text.to_dict() for text in buffer.storage]
 
     @staticmethod
-    def save_to_file(buffer: Buffer, filename: str):
+    def get_filename(filename: str | None) -> str:
+        """Get filename from user input if not provided, and ensure it has a .json extension."""
+
+        if filename is None:
+            filename = input("Enter a filename: ")
+
+        if not filename.lower().endswith(".json"):
+            filename += ".json"
+
+        return filename
+
+    @staticmethod
+    def save_to_file(buffer: Buffer, filename: str, mode: str = "a") -> None:
         """Saving data from buffer to file."""
 
         data_to_save = FileHandler.buffer_to_dict(buffer)
+        filename = FileHandler.get_filename(filename)
+
+        if mode not in ["w", "a"]:
+            print("Invalid mode. Setting up to default 'append' mode.")
+            mode = "a"
 
         try:
-            with open(filename, "a") as outfile:
+            with open(filename, mode) as outfile:
                 json.dump(data_to_save, outfile, indent=4)
                 outfile.write("\n")
+            print(f"Data successfully saved to {filename}")
         except FileNotFoundError:
-            pass
+            print(f"File {filename} not found.")
 
     @staticmethod
     def load_from_file(buffer: Buffer, filename: str):

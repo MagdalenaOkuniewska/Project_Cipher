a92565353b49485da3d86bbf87df281b27e086f7
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-03-18 14:37:31 +0100

    feat/no-ref/implement-manager

---
+++ manager/manager.py
@@ -0,0 +1,65 @@
+from cipher.cipher import CipherFacade, CipherNotFoundError
+from buffer.buffer import Buffer
+from files_service.file_handler import FileHandler
+from menu.menu_items import MainMenu
+
+
+class Manager:
+    """Manager class that acts as a Facade for the application.
+    Coordinates interactions between Cipher, Buffer, FileHandler and Menu components."""
+
+    def __init__(
+        self,
+        cipher_facade: CipherFacade,
+        buffer: Buffer,
+        file_handler: FileHandler,
+        menu: MainMenu,
+    ) -> None:
+        self.cipher_facade = cipher_facade
+        self.buffer = buffer
+        self.file_handler = file_handler
+        self.menu = menu
+
+    def encrypt_text(self):
+        """Handle encryption and stores in a buffer."""
+
+        print("\n=== Encrypt text ===")
+
+        text = input("Enter text to encrypt: ")
+        cipher_type = input("Enter the cipher type: ")
+
+        try:
+            cipher = self.cipher_facade.check_cipher_type(cipher_type)
+            encrypted_text = cipher.encrypt(text)
+            self.buffer.add(
+                content=encrypted_text, rot_type=cipher_type, status="encrypted"
+            )
+
+            print(f"Text encrypted successfully: {encrypted_text}")
+            print("Added to buffer with status 'encrypted'")
+
+        except CipherNotFoundError as e:
+            print(f"Cipher error: {str(e)}")
+        except Exception as e:
+            print(f"Error during encryption: {str(e)}")
+
+    # def decrypt_text(self):
+    #     pass
+    #
+    # def display_buffer(self):
+    #     pass
+    #
+    # def clear_buffer(self):
+    #     pass
+    #
+    # def save_to_file(self):
+    #     pass
+    #
+    # def load_from_file(self):
+    #     pass
+    #
+    # def exit_program(self):
+    #     pass
+    #
+    # def menu_choice(self):
+    #     pass

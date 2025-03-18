ad4de4f50e5abaf6e69f9119406c7bb94ad247a4
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-03-18 16:45:42 +0100

    feat/no-ref/implement-manager

--- manager/manager.py
+++ manager/manager.py
@@ -26,8 +26,8 @@ class Manager:
 
         print("\n=== Encrypt text ===")
 
-        text = input("Enter text to encrypt: ")
-        cipher_type = input("Enter the cipher type: ")
+        text: str = input("Enter text to encrypt: ")
+        cipher_type: str = input("Enter the cipher type: ")
 
         try:
             cipher = self.cipher_facade.check_cipher_type(cipher_type)
@@ -49,8 +49,8 @@ class Manager:
 
         print("\n=== Decrypt text ===")
 
-        text = input("Enter text to decrypt: ")
-        cipher_type = input("Enter the cipher type: ")
+        text: str = input("Enter text to decrypt: ")
+        cipher_type: str = input("Enter the cipher type: ")
 
         try:
             cipher = self.cipher_facade.check_cipher_type(cipher_type)
@@ -77,16 +77,38 @@ class Manager:
         """Clears current buffer content"""
 
         self.buffer.clear_all()
-        print("Buffer content cleared")
-
+        print("Buffer content cleared.")
 
     def save_to_file(self):
         """Save current buffer content to a JSON file"""
-        pass
+
+        print("\n=== Save to JSON file ===")
+
+        if not self.buffer.storage:
+            print("Buffer is empty. Nothing to save.")
+            return
+
+        filename: str = input("Enter filename to save to: ")
+        mode: str = input("Enter mode ('w' for write, 'a' for append): ")
+
+        try:
+            self.file_handler.save_to_file(self.buffer, filename, mode)
+            print(f"Buffer content successfully saved to {filename} in {mode} mode.")
+        except Exception as e:
+            print(f"An error occurred while saving to file: {e}")
 
     def load_from_file(self):
         """Load data from JSON file to a buffer"""
-        pass
+
+        print("\n=== Load from JSON file ===")
+
+        filename: str = input("Enter filename to load from: ")
+
+        try:
+            self.file_handler.load_from_file(self.buffer, filename)
+            print(f"Data successfully loaded from {filename}.")
+        except Exception as e:
+            print(f"An error occurred while loading from file: {e}")
 
     def exit_program(self):
         """Exit the application."""
@@ -97,6 +119,10 @@ class Manager:
     def menu_choice(self):
         """Map the menu choice to the appropriate function."""
 
+        self.menu.display_menu()
+
+        choice = self.menu.get_choice()
+
         actions = {
             1: self.encrypt_text,
             2: self.decrypt_text,
@@ -104,5 +130,10 @@ class Manager:
             4: self.clear_buffer,
             5: self.save_to_file,
             6: self.load_from_file,
-            7: self.exit_program
+            7: self.exit_program,
         }
+
+        if choice in actions:
+            return actions[choice]()
+        else:
+            print(f"Invalid choice: {choice}")

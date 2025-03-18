a1a0c5bf12c301c1ed7f7828df61545e4ac04540
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-03-18 13:16:07 +0100

    feat/no-ref/implement-menu

--- menu/menu_items.py
+++ menu/menu_items.py
@@ -22,11 +22,21 @@ class MainMenu(Menu):
             "Exit",
         ]
 
-    def display_menu(self):
+    def display_menu(self) -> None:
         print("\n===MENU===")
         for i, item in enumerate(self.menu_items, start=1):
             print(f"{i}. {item}")
         print("==========")
 
+    def get_choice(self) -> int:
+        """Get and validate user menu choice."""
 
-# def get_choice
+        while True:
+            try:
+                choice = int(input("Please provide a menu choice (number): "))
+                if 1 <= choice <= len(self.menu_items):
+                    return choice
+                else:
+                    print(f"Please enter a number between 1 and {len(self.menu_items)}")
+            except ValueError:
+                print("Please enter a valid number")
